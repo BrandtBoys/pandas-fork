b4cb6c642e5fd7f18997559aabc0aa61bdddd3c7
palleBrandt <lupa@itu.dk>
2025-04-26 16:19:08 +0200

    Added detect_language.py to the test environment

---
+++ detect_language.py
@@ -0,0 +1,36 @@
+import os
+
+# Mapping of file extensions to programming languages
+EXTENSION_TO_LANGUAGE = {
+    ".py": "python",
+    ".java": "java",
+    ".js": "javascript",
+    ".ts": "typescript",
+    ".c": "c",
+    ".cpp": "cpp",
+    ".cs": "csharp",
+    ".rb": "ruby",
+    ".php": "php",
+    ".go": "go",
+    ".rs": "rust",
+    ".swift": "swift",
+    ".kt": "kotlin",
+    ".sh": "shell",
+    ".html": "html",
+    ".css": "css",
+    ".json": "json",
+    ".xml": "xml",
+    ".yaml": "yaml",
+    ".yml": "yaml",
+    ".r": "r",
+    ".pl": "perl",
+    ".lua": "lua",
+    ".dart": "dart",
+    ".scala": "scala",
+}
+
+def detect_language(filename):
+    """Detects programming language based on file extension."""
+    _, ext = os.path.splitext(filename)  # Extract file extension
+    return EXTENSION_TO_LANGUAGE.get(ext, None)  # Return language or "unknown"
+
